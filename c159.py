31909cefe29b6876430ec52f5109b4a25691cf2f
HRIDYANSHKATARIA <85940668+HRIDYANSHKATARIA@users.noreply.github.com>
2022-08-25 18:55:39 +0530

    Add files via upload

---
+++ c159.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+root.geometry("600x400")
+root.title("exception handling")
+enter = Enter(root)
+label = Label(root,text = "Enter Country")
+mylist2=["India","England","China","American","Mexico"]
